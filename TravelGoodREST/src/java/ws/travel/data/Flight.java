89daf94f8fd479bf6999a5b3765ac17d01a38cf4
monicaAC <monicaandreea.coman@gmail.com>
2014-11-22 16:16:14 +0100

    package ws.travel.data

---
+++ TravelGoodREST/src/java/ws/travel/data/Flight.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.travel.data;
+
+/**
+ *
+ * @author Moni
+ */
+public class Flight {
+    
+}
