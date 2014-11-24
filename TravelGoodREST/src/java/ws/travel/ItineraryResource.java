1f4328d1f7f937275c515cec7423967a0b670a7d
oguzdemir <oguz.demir@imona.com.tr>
2014-11-24 11:39:39 +0100

    base implementation of booking itinerary, it can be an example to start on

--- TravelGoodREST/src/java/ws/travel/ItineraryResource.java
+++ TravelGoodREST/src/java/ws/travel/ItineraryResource.java
@@ -4,7 +4,23 @@
  */
 package ws.travel;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.netbeans.j2ee.wsdl.lameduck.java.flight.BookFlightFault;
+import org.netbeans.j2ee.wsdl.lameduck.java.flight.CancelFlightFault;
+import ws.bank.creditcard.CreditCard;
+import ws.travel.data.FlightInfo;
+import ws.travel.data.HotelInfo;
+import ws.travel.data.Itinerary;
+import ws.travel.services.FlightService;
 
 /**
  *
@@ -12,6 +28,8 @@ import javax.ws.rs.Path;
  */
 @Path("users/{userid}/itinerary/{itineraryid}")
 public class ItineraryResource {
+    
+    private static final String ITINERARY_NOT_FOUND = "itinerary not found";
     
     /**
      * @GET
@@ -39,6 +57,46 @@ public class ItineraryResource {
      * @Path("book")
      * Implement booking itinerary.
      */
+     @POST
+     @Path("book")
+     @Consumes(MediaType.APPLICATION_XML)
+     @Produces(MediaType.APPLICATION_XML)
+     public Response bookItinerary(@PathParam("userid") String userid,
+                                   @PathParam("itineraryid") String itineraryid,
+                                   CreditCard ccInfo) {
+        Itinerary itinerary = ItineraryPool.getItinerary(userid, itineraryid);
+        if(itinerary == null) {
+            return Response.status(Status.NOT_FOUND)
+                           .entity(ITINERARY_NOT_FOUND)
+                           .build();
+        }
+        List<FlightInfo> confirmedFlights = new ArrayList<FlightInfo>();
+        for(FlightInfo flight : itinerary.getFlightInfos()) {
+            try {
+                boolean booked = FlightService.bookFlight(flight.getBookingNr(), ccInfo);
+                if(booked) {
+                    flight.setStatus("CONFIRMED");
+                    confirmedFlights.add(flight);
+                }
+            } catch (BookFlightFault ex) {
+                for(FlightInfo confirmedFlight : confirmedFlights) {
+                    try {
+                        boolean cancelled = FlightService.cancelFlight(confirmedFlight.getBookingNr(), confirmedFlight.getPrice() * 2, ccInfo);
+                        if(cancelled) {
+                            flight.setStatus("CANCELLED");
+                        }
+                    } catch (CancelFlightFault ex1) {
+
+                    }
+                }
+            }
+       }
+
+        for(HotelInfo hotel : itinerary.getHotelInfos()) {
+
+        }
+        return Response.ok("OK").build();
+    }
     
     /**
      * @POST
