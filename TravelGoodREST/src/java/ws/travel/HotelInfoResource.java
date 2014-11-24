b5f01f711b7d983ee160c35d1c252f134cfb2d4f
paulinabien <bien.paulina@gmail.com>
2014-11-24 12:50:22 +0100

    addHotelToItinerary and addFlightToItinerary

--- TravelGoodREST/src/java/ws/travel/HotelInfoResource.java
+++ TravelGoodREST/src/java/ws/travel/HotelInfoResource.java
@@ -6,14 +6,16 @@ package ws.travel;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import ws.travel.ItineraryResource.ItineraryStatus;
 import ws.travel.data.HotelInfo;
 import ws.travel.data.Itinerary;
-import ws.travel.representation.StatusRepresentation;
+import ws.travel.representation.Link;
+import ws.travel.representation.Representation;
 
 /**
  *
@@ -22,7 +24,9 @@ import ws.travel.representation.StatusRepresentation;
 
 @Path("users/{userid}/itinerary/{itineraryid}/hotels/{bookingnum}")
 public class HotelInfoResource {
-    //itineraries
+    
+    public static final String BASE_URI = "http://localhost:8080/TravelGoodREST/webresources/";
+
     /**
      * 
      * @Path("add")
@@ -33,15 +37,55 @@ public class HotelInfoResource {
     @POST
     @Path("add")
     @Consumes(MediaType.APPLICATION_XML)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response addHotelToItinerary(String itineraryKey, HotelInfo hotel)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response addHotelToItinerary(@PathParam("userid") String userid,
+                                        @PathParam("itineraryId") String itineraryId,
+                                        HotelInfo hotel)
     {
-        StatusRepresentation statusRep = new StatusRepresentation();
-        Itinerary itinerary = ItineraryPool.getItinerary(itineraryKey);
-        //if itinerary is booked or cancelled
+        Itinerary itinerary = ItineraryPool.getItinerary(userid, itineraryId);
+        
+        //itinerary doesn't exist
+        if (itinerary == null)
+        {
+                return Response.
+                    status(Response.Status.NOT_FOUND).
+                    entity("Itinerary not found").
+                    build();
+        }
+        
+        //itinerary is already booked or cancelled
+        if (itinerary.getStatus().equals(ItineraryStatus.UNCONFIRMED.toString()) == false)
+        {
+                return Response.
+                    status(Response.Status.FORBIDDEN).
+                    entity("Itinerary is already booked or cancelled").
+                    build();
+        }
+        
         itinerary.addHotelToItinerary(hotel);
-        return Response.ok(statusRep).build();
+        return Response.ok("OK").build();
     }
+    
+    
+    //add cancel link
+    static void addCancelLink(String userId, String itineraryId, Representation response) {
+        Link link = new Link();
+        link.setUri(String.format("users/%s/itinerary/%s/cancel", BASE_URI, userId, itineraryId));
+        //link.setRel(CANCEL_RELATION);
+        response.getLinks().add(link);
+    }
+    
+    //add book link
+    static void addBookLink(String userId, String itineraryId, Representation response) {
+        Link link = new Link();
+        link.setUri(String.format("users/%s/itinerary/%s/book", BASE_URI, userId, itineraryId));
+        //link.setRel(CANCEL_RELATION);
+        response.getLinks().add(link);
+    }
+    
+    //get flights
+    //get hotels
+    //getitinerary
     
     
 }
